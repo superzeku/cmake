63ee49e4dd715acdeefe5fc6c71dc2f26cb04037
unknown <maiquochuy100506@gmail.com>
2023-06-18 14:29:19 +0700

    them  folder cmake04

---
+++ build/CMakeFiles/directory_test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/directory_test.dir/src/test1.cpp.obj"
+  "CMakeFiles/directory_test.dir/src/test1.cpp.obj.d"
+  "CMakeFiles/directory_test.dir/src/test2.cpp.obj"
+  "CMakeFiles/directory_test.dir/src/test2.cpp.obj.d"
+  "directory_test.exe"
+  "directory_test.exe.manifest"
+  "directory_test.pdb"
+  "libdirectory_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/directory_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/test1.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+int main(){
+    std::cout<<"test1"<<std::endl;
+}
